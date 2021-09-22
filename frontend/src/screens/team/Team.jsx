af14e4c1ecc389167834fbb997e0d1a2e3fd5aea
khushbuparmar25 <66319265+khushbuparmar25@users.noreply.github.com>
2021-09-23 00:15:51 +0530

    team page commit

---
+++ frontend/src/screens/team/Team.jsx
@@ -0,0 +1,164 @@
+import React from 'react';
+import './Team.css';
+import Navbar from '../../components/navbar/Navbar';
+
+const Team = () => {
+    return (
+        <div className="bg-gray-50 dark:bg-gray-800">
+            <Navbar />
+            <h1>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-20">
+                    <h1 className="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest dark:text-gray-100">OUR TEAM</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base dark:text-gray-200">Talent wins games, but teamwork and intelligence win championships. Teamwork is the ability to work together toward a common vision.</p>
+                    </div>
+                    <div className="flex flex-wrap -m-4">
+                    <div className="p-4 lg:w-1/2">
+                        <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                        <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200"/>
+                        <div className="flex-grow sm:pl-8">
+                            <h2 className="title-font font-medium text-lg text-gray-900 dark:text-gray-100">Bhagavatiraj Yadav</h2>
+                            <h3 className="text-gray-500 mb-3 dark:text-gray-200">FullStack Developer</h3>
+                            <p className="mb-4 dark:text-gray-300">Caption about the person</p>
+                            
+                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">        
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">
+                                    <i className="fa fa-envelope fontawesom-icon"></i>                                    
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">                                    
+                                    <i className="fa fa-github fontawesom-icon"></i>                                    
+                                </a>
+
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fab fa-linkedin-in fontawesom-icon"></i>                       
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fas fa-globe-asia fontawesom-icon"></i>                       
+                                </a>
+                            </span>
+                        </div>
+                        </div>
+                    </div>
+                                        
+                    <div className="p-4 lg:w-1/2">
+                        <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                        <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200"/>
+                        <div className="flex-grow sm:pl-8">
+                            <h2 className="title-font font-medium text-lg text-gray-900 dark:text-gray-100">Hersh Vitekar</h2>
+                            <h3 className="text-gray-500 mb-3 dark:text-gray-200">Fullstack Developer</h3>
+                            <p className="mb-4 dark:text-gray-300">Caption about the person</p>
+                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">        
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">
+                                    <i className="fa fa-envelope fontawesom-icon"></i>                                    
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">                                    
+                                    <i className="fa fa-github fontawesom-icon"></i>                                    
+                                </a>
+
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fab fa-linkedin-in fontawesom-icon"></i>                       
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fas fa-globe-asia fontawesom-icon"></i>                       
+                                </a>
+                            </span>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="p-4 lg:w-1/2">
+                        <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                        <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200"/>
+                        <div className="flex-grow sm:pl-8">
+                            <h2 className="title-font font-medium text-lg text-gray-900 dark:text-gray-100">Khushbu Pamar</h2>
+                            <h3 className="text-gray-500 mb-3 dark:text-gray-200">Fullstack Developer</h3>
+                            <p className="mb-4 dark:text-gray-300">Caption about the person</p>
+                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">        
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">
+                                    <i className="fa fa-envelope fontawesom-icon"></i>                                    
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">                                    
+                                    <i className="fa fa-github fontawesom-icon"></i>                                    
+                                </a>
+
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fab fa-linkedin-in fontawesom-icon"></i>                       
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fas fa-globe-asia fontawesom-icon"></i>                       
+                                </a>
+                            </span>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="p-4 lg:w-1/2">
+                        <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                        <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200"/>
+                        <div className="flex-grow sm:pl-8">
+                            <h2 className="title-font font-medium text-lg text-gray-900 dark:text-gray-100">Rishabh Patil</h2>
+                            <h3 className="text-gray-500 mb-3 dark:text-gray-200">Fullstack Developer</h3>
+                            <p className="mb-4 dark:text-gray-300">Caption about the person</p>
+                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">        
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">
+                                    <i className="fa fa-envelope fontawesom-icon"></i>                                    
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">                                    
+                                    <i className="fa fa-github fontawesom-icon"></i>                                    
+                                </a>
+
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fab fa-linkedin-in fontawesom-icon"></i>                       
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fas fa-globe-asia fontawesom-icon"></i>                       
+                                </a>
+                            </span>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="p-4 lg:w-1/2">
+                        <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                        <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200"/>
+                        <div className="flex-grow sm:pl-8">
+                            <h2 className="title-font font-medium text-lg text-gray-900 dark:text-gray-100">Shubh Nisar</h2>
+                            <h3 className="text-gray-500 mb-3 dark:text-gray-200">Fullstack Developer</h3>
+                            <p className="mb-4 dark:text-gray-300">Caption about the person</p>
+                            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">        
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">
+                                    <i className="fa fa-envelope fontawesom-icon"></i>                                    
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300">                                    
+                                    <i className="fa fa-github fontawesom-icon"></i>                                    
+                                </a>
+
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fab fa-linkedin-in fontawesom-icon"></i>                       
+                                </a>
+                                
+                                <a className="ml-3 text-gray-500 dark:text-gray-300"> 
+                                    <i className="fas fa-globe-asia fontawesom-icon"></i>                       
+                                </a>
+                                
+                            </span>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
+            </h1>
+        </div>
+    );
+};
+
+export default Team;
