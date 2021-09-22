7da573df9a64317b1c6a757c6cb691a38b4c8dfb
khushbuparmar25 <66319265+khushbuparmar25@users.noreply.github.com>
2021-09-22 22:52:34 +0530

    home commit

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,11 +1,15 @@
 import { createBrowserHistory } from "history";
 import { Router, Switch, Route, BrowserRouter } from "react-router-dom";
+import Home from "./screens/home/Home";
+
 const Routes = (props) => {
   const history = createBrowserHistory();
 
   return (
     <BrowserRouter history={history}>
-      <Switch></Switch>
+      <Switch>        
+        <Route path="/home" component={Home} exact />
+      </Switch>
     </BrowserRouter>
   );
 };
