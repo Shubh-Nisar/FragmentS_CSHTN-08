fe969cc6da0bfdd29a291f8eaba1c086773e8ef5
khushbuparmar25 <66319265+khushbuparmar25@users.noreply.github.com>
2021-09-23 11:57:28 +0530

    course commit

--- frontend/src/GlobalContext.js
+++ frontend/src/GlobalContext.js
@@ -1,4 +1,7 @@
 import { createContext, useEffect, useState } from "react";
+import Fuse from "fuse.js";
+
+import reactPng from "./assets/images/react.png";
 
 export const GlobalContext = createContext();
 
@@ -7,11 +10,68 @@ export const GlobalProvider = (props) => {
   const [theme, setTheme] = useState(
     localStorage.getItem("edtech-theme") === true ? true : false
   );
+  const [courseSelect, setCourseSelect] = useState({
+    id: 1,
+    name: "ReactJS",
+    avatar: reactPng,
+  });
+  const [allCourses, setAllCourses] = useState([]);
+  const [courseSearch, setCourseSearch] = useState("");
+  const [courseSearchResult, setCourseSearchResult] = useState(allCourses);
+
+  useEffect(() => {
+    console.log(theme);
+    console.log(localStorage.getItem("edtech-theme"));
+    if (theme) {
+      document.documentElement.classList.remove("dark");
+      setTheme(true);
+    } else {
+      document.documentElement.classList.add("dark");
+      setTheme(false);
+    }
+  }, [theme]);
+
+  useEffect(() => {
+    console.log(courseSelect);
+    const arr = [];
+    allCourses.map(
+      (elem) => elem.category === courseSelect.name && arr.push(elem)
+    );
+    console.log(arr);
+    setCourseSearchResult(arr);
+  }, [courseSelect]);
+
+  useEffect(() => {
+    console.log(allCourses);
+  }, [allCourses]);
+
+  useEffect(() => {
+    console.log(courseSearch);
+    console.log(allCourses);
+    const options = {
+      includeScore: true,
+      // Search in `author` and in `tags` array
+      keys: ["title"],
+    };
+
+    const fuse = new Fuse(allCourses, options);
+    const result = fuse.search(courseSearch);
+    console.log(result);
+    if (result && result.length === 0) {
+      setCourseSearchResult(allCourses);
+    } else {
+      setCourseSearchResult([...result.map((elem) => elem.item)]);
+    }
+  }, [courseSearch]);
   return (
     <GlobalContext.Provider
       value={{
         user: [userData, setUserData],
         siteTheme: [theme, setTheme],
+        courseSelection: [courseSelect, setCourseSelect],
+        courseAll: [allCourses, setAllCourses],
+        searchCourse: [courseSearch, setCourseSearch],
+        searchCourseResult: [courseSearchResult, setCourseSearchResult],
       }}
     >
       {props.children}
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,7 @@ import { Router, Switch, Route, BrowserRouter } from "react-router-dom";
 import Home from "./screens/home/Home";
 import Team from "./screens/team/Team";
 import Courses from "./screens/courses/Courses";
+import CourseOverview from "./screens/courses/CourseOverview";
 
 const Routes = (props) => {
   const history = createBrowserHistory();
@@ -13,6 +14,7 @@ const Routes = (props) => {
         <Route path="/home" component={Home} exact />
         <Route path="/team" component={Team} exact />
         <Route path="/courses" component={Courses} exact />
+        <Route path="/courses/:id" component={CourseOverview} exact />
       </Switch>
     </BrowserRouter>
   );
