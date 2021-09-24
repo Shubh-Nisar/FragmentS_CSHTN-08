2b4dd9632e10b1bd43a27085e3c01e70e512a6b7
Shubh Nisar <shubh_ubuntu@shubh.ubuntu-Lenovo-ideapad-330S-15IKB>
2021-09-24 10:59:11 +0530

    UI Updates

--- frontend/src/screens/signUp/registerStyles.js
+++ frontend/src/screens/signUp/registerStyles.js
@@ -1,24 +1,24 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useRegisterStyles = makeStyles((theme) => ({
   root: {
-    height: 'auto',
+    height: "auto",
   },
   button: {
     margin: theme.spacing(1),
   },
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing(1),
     paddingTop: theme.spacing(2),
     borderTop: `1px solid ${theme.palette.divider}`,
@@ -27,13 +27,13 @@ const useRegisterStyles = makeStyles((theme) => ({
     margin: theme.spacing(3, 0, 2),
   },
   hidden: {
-    display: 'none',
+    display: "none",
   },
   show: {
-    display: 'block',
-    width: '100%',
-    textAlign: 'center',
-    color: '#ff0000',
+    display: "block",
+    width: "100%",
+    textAlign: "center",
+    color: "#ff0000",
   },
 }));
 
